4cacee3db440f558e3cfc05482406947e113f9e5
mario <kinuthiamariodavis@gmail.com>
2023-09-26 10:33:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Code your solution here
-findMatching
-string
+function findMatching(drivers, name) {
+    return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+  }
 
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter((driver) =>
+      driver.toLowerCase().startsWith(letters.toLowerCase())
+    );
+  }
 
-fuzzyMatch  
-string
+  function matchName(drivers, name) {
+    return drivers.filter((driver) => driver.name === name);
+  }
